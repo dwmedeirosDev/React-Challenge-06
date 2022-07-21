c0cd5ce6ee462751440d1155b4b0cf71e18fde1a
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-07-21 09:51:11 -0300

    Done

---
+++ README.txt
@@ -0,0 +1,4 @@
+// Criar um contador com setState
+
+// 1 - O contador não pode chegar abaixo de 0
+// 2 - O contador não pode chegar acima de 10
--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+    state = {
+        number: 0
+    }
+    
+    somar = () => {
+        if(this.state.number <= 9){
+            this.setState({
+                number: this.state.number + 1
+            })
+        }
+    }
+
+    subtrair = () => {
+        if(this.state.number >= 1){
+            this.setState({
+                number: this.state.number - 1
+            })
+        }
+    }
+
+    render(){
+        return(
+            <div className="card">
+                <h1>Contador</h1>
+                <h2>{this.state.number}</h2>
+                <nav>
+                    <button onClick={this.somar}>+</button>
+                    <button onClick={this.subtrair}>-</button>
+                </nav>
+            </div>
+        )
+    }
+
 }
-
-export default App;
